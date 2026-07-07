9ab38bd3f0e786785d7f02e7f50bbc2537aba8ab
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-07 14:55:24 +0200

    Make the input be displayed in correct form
    OMG, the code actually works. It takes a number
    then an operator and then a second number
    and actually does the needed function.
    IT is still just in console.log. Have to make It
    be displayed in the input box.

--- script.js
+++ script.js
@@ -13,24 +13,24 @@ for (let button of buttons) {
 returnButtons()
 
 function handleInput(buttonValue) {
-    if (Number.isInteger(+buttonValue) && waitingFor == "firstNumber") {
+    if (isInteger(buttonValue) && waitingFor == "firstNumber") {
         firstNumber += buttonValue
     }
-    else if (isOperator(buttonValue)) {
+    if (isOperator(buttonValue)) {
         myOperator = buttonValue
         waitingFor = "secondNumber"
     }
-    else if (waitingFor == "secondNumber" && Number.isInteger(+buttonValue)) {
+    if (waitingFor == "secondNumber" && isInteger(buttonValue)) {
         secondNumber += buttonValue
     }
-    else if (buttonValue == "=") {
+    if (buttonValue == "=") {
         waitingFor = "reset"
-        if (myOperator == "+") console.log (sum(+firstNumber, +secondNumber))
-        else if (myOperator == "-") console.log (subtract(+firstNumber, +secondNumber))
-        else if (myOperator == "x") console.log (multiply(+firstNumber, +secondNumber))
-        else if (myOperator == "/") console.log (divide(+firstNumber, +secondNumber))
+        if (myOperator == "+") console.log (`${firstNumber} + ${secondNumber} = ${sum(+firstNumber, +secondNumber)}`)
+        else if (myOperator == "-") console.log (`${firstNumber} - ${secondNumber} = ${subtract(+firstNumber, +secondNumber)}`)
+        else if (myOperator == "x") console.log (`${firstNumber} x ${secondNumber} = ${multiply(+firstNumber, +secondNumber)}`)
+        else if (myOperator == "/") console.log (`${firstNumber} / ${secondNumber} = ${divide(+firstNumber, +secondNumber)}`)
     }
-    else if (waitingFor == "reset") {
+    if (waitingFor == "reset") {
         myOperator = ""
         firstNumber = ""
         secondNumber = ""
@@ -45,6 +45,14 @@ function isOperator(str) {
         return true
     }
 }
+function isInteger(str) {
+    const integers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    if (integers.includes(str)){
+        return true
+    }
+}
+
+
 
 
 function subtract(a, b) {
