8b53fc5bc9378632ffb8d93c19218256e8815fca
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-07 08:24:09 +0200

    Figure out connecting the handling functions

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
 const buttons = document.querySelectorAll("button")
+let firstNumber = ""
+let secondNumber = ""
+let myOperator = ""
+waitingFor = "firstNumber"
 
 function returnButtons() {
 for (let button of buttons) {
     button.addEventListener("click", () => {
-        const number = +button.textContent
-        if (Number.isInteger(number)) {
-            console.log(number)
-            return number
-        }
-        else return button.textContent
+        handleInput(+button.textContent)
     })
 }}
-function returnButtons() {
-for (let button of buttons) {
-    button.addEventListener("click", () => {
-        const number = +button.textContent
-        if (Number.isInteger(number)) {
-            console.log(number)
-            return number
-        }
-        else return button.textContent
-    })
-}}
-let firstNumber = ""
-let secondNumber = ""
-let myOperator = ""
+
 function handleInput(buttonValue) {
-    
-    waitingFor = "firstNumber"
+
     if (Number.isInteger(buttonValue) && waitingFor == "firstNumber") {
         firstNumber += buttonValue
+        console.log(firstNumber)
     }
     else if (!Number.isInteger(buttonValue)) {
         myOperator = button.value
@@ -38,7 +24,9 @@ function handleInput(buttonValue) {
     if (waitingFor == "secondNumber" && Number.isInteger(buttonValue)) {
         secondNumber += buttonValue
     }
-    console.log(firstNumber, myOperator, secondNumber)
+    else if (!Number.isInteger(buttonValue)) {
+        console.log(firstNumber, myOperator, secondNumber)
+    }
 }
 
 
