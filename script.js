bb035cd222fec53c12cb9b17d64383c9826b31b5
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-07 08:39:59 +0200

    Make getButtons() return inputs in correct order
    getButtons() returns firstNumber, the operator and
    the secondNumber

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-const buttons = document.querySelectorAll("button")
 let firstNumber = ""
 let secondNumber = ""
 let myOperator = ""
 waitingFor = "firstNumber"
 
 function returnButtons() {
+const buttons = document.querySelectorAll("button")
 for (let button of buttons) {
     button.addEventListener("click", () => {
-        handleInput(+button.textContent)
+        handleInput(button.textContent)
     })
 }}
+returnButtons()
 
 function handleInput(buttonValue) {
-
-    if (Number.isInteger(buttonValue) && waitingFor == "firstNumber") {
+    if (Number.isInteger(+buttonValue) && waitingFor == "firstNumber") {
         firstNumber += buttonValue
-        console.log(firstNumber)
     }
-    else if (!Number.isInteger(buttonValue)) {
-        myOperator = button.value
+    else if (!Number.isInteger(+buttonValue)) {
+        myOperator = buttonValue
         waitingFor = "secondNumber"
     }
-    if (waitingFor == "secondNumber" && Number.isInteger(buttonValue)) {
+    if (waitingFor == "secondNumber" && Number.isInteger(+buttonValue)) {
         secondNumber += buttonValue
     }
-    else if (!Number.isInteger(buttonValue)) {
+    else if (!Number.isInteger(+buttonValue)) {
         console.log(firstNumber, myOperator, secondNumber)
     }
 }
 
 
 
-function operator() {
-    const button = returnButtons()
-    if (button = "+") {
-        console.log(sum(button, button))
-    }
-}
+
 
 function sum(a, b) {
     return a+b
