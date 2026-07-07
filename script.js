d04fa62a885dca8d1105812d0aa7a1cf358db133
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-07-07 08:54:12 +0200

    Make isOperator() that checks if input is operator

--- script.js
+++ script.js
@@ -16,18 +16,28 @@ function handleInput(buttonValue) {
     if (Number.isInteger(+buttonValue) && waitingFor == "firstNumber") {
         firstNumber += buttonValue
     }
-    else if (!Number.isInteger(+buttonValue)) {
+    else if (isOperator(buttonValue)) {
         myOperator = buttonValue
         waitingFor = "secondNumber"
     }
-    if (waitingFor == "secondNumber" && Number.isInteger(+buttonValue)) {
+    else if (waitingFor == "secondNumber" && Number.isInteger(+buttonValue)) {
         secondNumber += buttonValue
     }
-    else if (!Number.isInteger(+buttonValue)) {
+    else if (buttonValue == "=") {
         console.log(firstNumber, myOperator, secondNumber)
+        waitingFor = "firstNumber"
+        firstNumber = ""
+        secondNumber = ""
+        myOperator = ""
     }
 }
 
+function isOperator(str) {
+    const operators = ["+", "-", "x", "/"]
+    if (operators.includes(str)) {
+        return true
+    }
+}
 
 
 
